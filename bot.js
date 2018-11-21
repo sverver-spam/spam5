477b462f071ea4fd0eeef41feb3c63b10c30f8ac
sverver-spam <45228711+sverver-spam@users.noreply.github.com>
2018-11-21 11:37:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,14 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
+console.log("Scrpit By Dream");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
-});
 
-client.on('ready', async() => {
-var server = "510562350177320960"; // ايدي السررفر
-var channel = "510562519056646156";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
-    },305);
+client.on("ready", () => {
+let channel =     client.channels.get("514729634030551051")
+setInterval(function() {
+channel.send(`spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam `);
+}, 25)
 })
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
  
-client.login(NTA4MDI1MjU2ODcxNDYwODc0.Ds7Ptg.UtUY60UF937mieeNwb0c5KBJP1o);
+ 
+client.login('NTE0NzMyNjAwMzU5OTExNDI1.Dta2Rw.izKGV-KpIziAt4epr0XD_8XZfig');
