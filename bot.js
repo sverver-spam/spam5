e3b137a5deafcf6331f8850582828ac5e6e4f83e
sverver-spam <45228711+sverver-spam@users.noreply.github.com>
2018-11-21 15:25:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,12 @@ channel.send(`spam spam spam spam spam spam spam spam spam spam spam spam spam s
 }, 25)
 })
  
-
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "say") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
 client.login('NTE0NzMyNjAwMzU5OTExNDI1.Dta2Rw.izKGV-KpIziAt4epr0XD_8XZfig');
