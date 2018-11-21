1c3881f06d71138d257c3b0a19a70d94a0be69cb
sverver-spam <45228711+sverver-spam@users.noreply.github.com>
2018-11-21 16:07:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on("message", function(message) {
     var args = message.content.split(/ +/g);
     var command = args.shift()
     
-    if(command == "say") {
+    if(command == "5say") {
         message.channel.send(args.slice(1, args.length).join(" "))    
     }
 });
