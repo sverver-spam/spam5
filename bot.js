468021785309067b23140eea7cf69402bf12c5c0
sverver-spam <45228711+sverver-spam@users.noreply.github.com>
2018-11-21 14:28:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
-
+case 'say': if (message.startsWith("!say") == true) {//Check if the message send starts with "say" var newMessage = message.replace("say ", "");//Making a variable where "say " is removed bot.sendMessage({to: channelID, message:newMessage})//Send the new variable. } break;
 client.on("ready", () => {
 let channel =     client.channels.get("514729634030551051")
 setInterval(function() {
